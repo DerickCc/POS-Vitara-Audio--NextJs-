46161ed039509700d0044151997d1c66fa370c95
DerickCc <derickchainatra@gmail.com>
2024-09-22 03:25:08 +0700

    update db, prisma, and package

--- src/models/supplier.model.ts
+++ src/models/supplier.model.ts
@@ -10,7 +10,7 @@ export const SupplierSchema = z.object({
       message: 'No. Telepon harus berupa angka',
     }),
   address: z.string().max(100, { message: 'Alamat tidak boleh lebih dari 100 huruf' }).optional().nullable(),
-  remarks: z.string().max(150, { message: 'Keterangan tidak boleh lebih dari 150 huruf' }).optional().nullable(),
+  remarks: z.string().max(250, { message: 'Keterangan tidak boleh lebih dari 150 huruf' }).optional().nullable(),
   receivables: z.coerce.number().min(0, { message: "Piutang harus berupa angka positif" }),
   receivablesLimit: z.coerce.number().min(0, { message: "Limit Piutang harus berupa angka positif" }),
 })
