ec30d6003f0a290a39bb3b4d53dc28b5aaf02c41
DerickCc <derickchainatra@gmail.com>
2025-01-06 22:24:28 +0700

    fix minor bug, remove unused package

--- src/config/global-variables.ts
+++ src/config/global-variables.ts
@@ -23,16 +23,16 @@ export const pageSizeOptions = [
     label: '5',
   },
   {
-    value: 2,
-    label: '2',
+    value: 10,
+    label: '10',
   },
   {
-    value: 3,
-    label: '3',
+    value: 15,
+    label: '15',
   },
   {
-    value: 1,
-    label: '1',
+    value: 25,
+    label: '25',
   },
   {
     value: 50,
